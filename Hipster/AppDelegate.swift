93f3fe11c076525e4740db9837f939fe2f6dc24f
Moshe <moshberm@gmail.com>
2015-04-13 06:12:28 -0400

    Initial Commit

---
+++ Hipster/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hipster
+//
+//  Created by Moshe Berman on 4/13/15.
+//  Copyright (c) 2015 Moshe Berman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hipster/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hipster
+//
+//  Created by Moshe Berman on 4/13/15.
+//  Copyright (c) 2015 Moshe Berman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
